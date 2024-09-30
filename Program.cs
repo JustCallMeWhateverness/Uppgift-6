a9f810573a7aabe82c7fd09f9cd0b6427e1cda31
Nicole Fritz <nicolefritz00@gmail.com>
2024-09-30 10:37:11 +0200

    Initial Commit: Created a list with names & a search function

--- Program.cs
+++ Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace Uppgift_6
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            List<string> names = new List<string> { "Aelin", "Dorian", "Freye", "Sam", "Eira" };
+            Console.WriteLine("Original list:");
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+            names.Sort();
+            Console.WriteLine("\nSorted list:");
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("\nEnter name to search:");
+            string searchName = Console.ReadLine();
+            if (names.Contains(searchName))
+            {
+                Console.WriteLine($"{searchName} is in the list.");
+            }
+            else
+            {
+                Console.WriteLine($"{searchName} is not in the list.");
+            }
+
+            Console.ReadKey();
+
         }
     }
 }

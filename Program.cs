de4eec5525ba698981ae926452ad999e253b193e
Nicole Fritz <nicolefritz00@gmail.com>
2024-10-01 18:55:49 +0200

    New Commit 4: Added exception handling with wrong user input

--- Program.cs
+++ Program.cs
@@ -15,10 +15,52 @@ namespace Uppgift_6
         public static void AddPerson(Dictionary<string, int> myDictionary)
         {
             Console.WriteLine("\nAdd a new person");
-            Console.WriteLine("Enter name:");
-            string name = Console.ReadLine();
-            Console.WriteLine("Enter age:");
-            int age = int.Parse(Console.ReadLine());
+
+            string name = "";
+            while (true)
+            {
+                Console.WriteLine("Enter name:");
+                name = Console.ReadLine();
+
+                //Fångar upp om inmatningen är tom
+                if (string.IsNullOrWhiteSpace(name))
+                {
+                    Console.WriteLine("Name cannot be empty. Enter a valid name");
+                }
+                //Fångar upp om användaren skriver in ett nummer
+                else if (int.TryParse(name, out _))
+                {
+                    Console.WriteLine("Names can't be numbers. Enter a valid name");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            int age = 0;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter age:");
+                    age = int.Parse(Console.ReadLine());
+                    //Om inmatningen är korrekt lämnar det loop
+                    if (age >= 0)
+                    {
+                        break;
+                    }
+                    //Fångar upp om inmatningen är ett negativt nummer
+                    else
+                    {
+                        Console.WriteLine("Age can't be negative. Enter a valid number");
+                    }
+                }
+                //Fångar upp om inmatningen ej är ett nummer
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Enter a valid number");
+                }
+            }
             myDictionary.Add(name, age);
         }
         //Skriver ut den nuvarande listan
@@ -30,27 +72,32 @@ namespace Uppgift_6
             {
                 Console.WriteLine("Name: {0} Age: {1}", kvp.Key, kvp.Value);
             }
-
         }
         //Användaren kan uppdatera en persons ålder
         public static void UpdatePersonAge(Dictionary<string, int> myDictionary)
         {
-            Console.WriteLine("Enter the persons name you wanna change the age of");
-            string name = Console.ReadLine();
-            Console.WriteLine($"Enter the new age for {name}");
-            int age = int.Parse(Console.ReadLine());
-
-            if (myDictionary.ContainsKey(name))
+            while (true)
             {
-                myDictionary[name] = age;
-
+                Console.WriteLine("Enter the persons name you wanna change the age of");
+                string name = Console.ReadLine();
+                //Kontrollerar om namnet finns
+                if (myDictionary.ContainsKey(name))
+                {
+                    Console.WriteLine($"Enter the new age for {name}");
+                    int age = int.Parse(Console.ReadLine());
+                    myDictionary[name] = age;
+                    break;
+                }
+                //Fångar upp om namnet inte finns
+                else if (!myDictionary.ContainsKey(name))
+                {
+                    Console.WriteLine("Sorry! That name does not exist here!Try again");
+                }
             }
-
         }
         //Sorterar personerna i alfabetiskt order
         public static void SortPeople(Dictionary<string, int> myDictionary)
         {
-
             var sortByName = myDictionary.OrderBy(kvp => kvp.Key);
 
             Console.WriteLine("\nSorted list:");
@@ -59,8 +106,6 @@ namespace Uppgift_6
             {
                 Console.WriteLine("Name: {0} Age: {1} ", kvp.Key, kvp.Value);
             }
-
-
         }
         //Användaren kan söka efter en person
         public static void SearchPerson(Dictionary<string, int> myDictionary)
@@ -90,7 +135,7 @@ namespace Uppgift_6
                 {"Sam",18},
                 {"Eira",12}
             };
-            int menu;
+            int menu = 0;
             Console.WriteLine("WELCOME TO THE LIST OF NAMES, WE HOPE YOU ENJOY YOUR STAY HERE :)");
             Console.WriteLine("\nOriginal list:");
             //Skriver ut alla namn & deras ålder med en foreach loop
@@ -99,7 +144,7 @@ namespace Uppgift_6
                 Console.WriteLine("Name: {0} Age: {1}", kvp.Key, kvp.Value);
             }
 
-            //skapar en meny med 6 olika alternativ så användaren själv kan välja vad den vill göra
+            //skapar en switch meny med 6 olika alternativ
             do
             {
                 Console.WriteLine("\nChoose one of the options below");
@@ -109,7 +154,16 @@ namespace Uppgift_6
                 Console.WriteLine("4:Sort people in alphabetical order");
                 Console.WriteLine("5:Search for a person");
                 Console.WriteLine("6:Exit");
-                menu = int.Parse(Console.ReadLine());
+
+                string input = Console.ReadLine();
+                bool validInput = int.TryParse(input, out menu);
+                //Fångar upp om användarens inmatning är felaktig
+                if (!validInput || menu < 1 || menu > 6)
+                {
+                    Console.WriteLine("Invalid input.Try again!");
+                    menu = -1;
+                    continue;
+                }
 
                 switch (menu)
                 {
@@ -131,14 +185,13 @@ namespace Uppgift_6
                     case 6:
                         menu = 0;
                         break;
-
+                    default:
+                        Console.WriteLine("Invalid choice.Try again");
+                        break;
                 }
-
             } while (menu != 0);
 
             Console.ReadKey();
-
         }
     }
 }
-
